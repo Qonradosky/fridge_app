651cd966ef6d3b62e2e3cf7bb1469eed4bf4c53a
Qonradosky <glowczynskikonrad@gmail.com>
2026-06-16 00:40:01 +0200

    Activate service worker updates immediately

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = "virtual-fridge-v2";
+const CACHE_NAME = "virtual-fridge-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
@@ -11,6 +12,7 @@ self.addEventListener("activate", (event) => {
       Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
     ),
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
