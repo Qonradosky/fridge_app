76babf09250d814ba6fe5f770740424d2ce0e7da
Qonradosky <glowczynskikonrad@gmail.com>
2026-06-16 00:35:36 +0200

    Bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "virtual-fridge-v1";
+const CACHE_NAME = "virtual-fridge-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
