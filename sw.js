f08717d46ac6dff8b415111b5d8e98e18bf55ad9
Qonradosky <glowczynskikonrad@gmail.com>
2026-06-16 00:43:12 +0200

    Group product categories

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "virtual-fridge-v3";
+const CACHE_NAME = "virtual-fridge-v4";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
